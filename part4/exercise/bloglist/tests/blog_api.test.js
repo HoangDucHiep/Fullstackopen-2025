bfcd98ae215f56d1f2378010d6c773b0767eacee
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-03-27 18:27:27 +0700

    part4 - 4.8 - 12

--- part4/exercise/bloglist/tests/blog_api.test.js
+++ part4/exercise/bloglist/tests/blog_api.test.js
@@ -28,3 +28,65 @@ test.only('there are 3 blogs', async t => {
     console.log(response.body.length)
     assert.strictEqual(response.body.length, 3)
 })
+
+test.only('blogs have id property', async t => { 
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+    assert.ok(blog.id)
+})
+
+test.only('a valid blog can be added', async t => {
+    const oldBlogs = await Blog.find({})
+
+    const newBlog = {
+        "title": "test",
+        "author": "Hoang Duc Hiep",
+        "url": "http://newurl.com",
+        "likes": 1000
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const blogsAtEnd = await Blog.find({})
+    assert.strictEqual(blogsAtEnd.length, oldBlogs.length + 1)
+    
+    // check if the new blog is in the list of blogs
+    const contents = blogsAtEnd.map(blog => blog.title)
+    assert.ok(contents.includes(newBlog.title))
+})
+
+test.only('if likes property is missing, it will default to 0', async t => {
+    const newBlog = {
+        "title": "test",
+        "author": "Hoang Duc Hiep",
+        "url": "http://newurl.com"
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const blog = response.body
+    assert.strictEqual(blog.likes, 0)
+})
+
+
+test.only('if title and url are missing, respond with 400', async t => {
+    const newBlog = {
+        "author": "Hoang Duc Hiep",
+        "likes": 1000
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+})
